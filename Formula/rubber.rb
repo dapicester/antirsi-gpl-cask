cb1de2b5408593f31ce7e3a2e626b361b6d2e686
Paolo D'Apice <paolo@sidechef.com>
2023-02-05 15:25:53 +0800

    Update rubber

--- Formula/rubber.rb
+++ Formula/rubber.rb
@@ -1,9 +1,9 @@
 class Rubber < Formula
   desc "Automated building of LaTeX documents"
   homepage "https://launchpad.net/rubber"
-  url "https://launchpad.net/rubber/trunk/1.5.1/+download/rubber-1.5.1.tar.gz"
-  version "1.5.1"
-  sha256 "37a843dc36a8aa256f9a66de130a031a0406346e663e1c257e45fd0a6eae0d9d"
+  url "https://gitlab.com/latex-rubber/rubber/-/archive/1.6.0/rubber-1.6.0.tar.bz2"
+  version "1.6.0"
+  sha256 "59c3907245b6bf2adcc2451d1b150e3f5ed193fa80c65cf883ae4a8f443b7dc9"
   depends_on "python3" => :build
 
   def install
